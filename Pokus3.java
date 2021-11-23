80735f7fe428104a1856785b5f948ee8b51fd419
Daniels-0 <63020950+Daniels-0@users.noreply.github.com>
2021-11-23 10:39:51 +0100

    Update Pokus3.java

--- Pokus3.java
+++ Pokus3.java
@@ -1,37 +1,41 @@
 import java.util.Scanner;
 import java.util.Random;
-public class Pokus3 {
+public class Skola {
     public static void main(String[] args) {
 
-        double x = 100;
-        double y;
-        int z;
 
-        while (x > 0){
-            for (int i = 0; i <= x; i++){
-                System.out.println("Zadaj čiastku: ");
+
+        float y = 100;
+        float x;
+        int o;
+
+        while (y > 0){
+
+            for (int i =0; i<=y;){
+                System.out.println("zadaj čiastku");
                 Scanner xa = new Scanner(System.in);
-                y = xa.nextDouble();
-                System.out.println("zadaj číslo 1 alebo 2");
-                Scanner scanner = new Scanner(System.in);
-                z = scanner.nextInt();
-                if(y >= x) {
-                    x = x - y;
+                x = xa.nextFloat();
+                System.out.println("číslo 1/2");
+                Scanner ya = new Scanner(System.in);
+                o = ya.nextInt();
+
+                if(y>=x) {
+                    y = y - x;
+                }
+                else{
+                    System.out.println("Nemáš dostatok peniazí");
                 }
 
-                else{
-                    System.out.println("Nemaš money");
-                }
                 Random rand = new Random();
                 int upperbound = 2;
                 int int_random = rand.nextInt(upperbound);
-                if (int_random == z){
-                    x = x +(2 * x);
+                if (int_random == o){
+                    y = y +(2 * x);
                 }
-                System.out.println("zostatok: "+ x);
+                System.out.println("zostatok :"+ y);
 
-            }if(x == 0){
-                System.out.println(" už nemáš peniaze ");
+            }if(y == 0){
+                System.out.println("Už nemáš peniaze");
             }
         }
     }
