9ce5a583b09390385e33bd58df1fcc4bfbb7f1cd
Daniels-0 <63020950+Daniels-0@users.noreply.github.com>
2022-02-02 11:19:16 +0100

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Cintent type" content="text/html; charset=UTF-8">
+        <title>Grc</title>
+    </head>
+    <body>
+    <p>Zadaj meno a heslo</p>
+
+<form method="POST"action="indexx.php">
+    <input name="meno" type="text"> <br>
+    <input password="heslo" type="text"> <br>
+    </form>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Registration</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<form method="POST"action="login">
+    <input name="meno" type="text">
+    <input password="heslo" type="text">
+    <input type="vypíš">
+</form>
+</body>
+</html>
